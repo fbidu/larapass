96fb9980b850b754c04903706aacf7425cb83bd7
Felipe Bidu <bidu.pub@gmail.com>
2015-07-17 09:24:48 -0300

    Creating the checkPassword function

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Hash;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class PasswordController extends BaseController
 {
     public function hashPassword($password)
     {
-        return $password;
+        return Hash::make($password);
+    }
+
+    public function checkPassword($password, $hash)
+    {
+        if (Hash::check($password, $hash))
+        {
+            return "Yay :D";
+        }
+        else
+        {
+            return "Naay :(";
+        }
     }
 }
