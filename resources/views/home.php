dfe737c66016e8964bacbc5db21a2a6c191c411d
fbidu <bidu.pub@gmail.com>
2015-07-17 14:16:49 -0700

    Fixing CDN and .htaccess issues

--- resources/views/home.php
+++ resources/views/home.php
@@ -4,7 +4,7 @@
         <title>Larapass</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
      
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="narrow.css">
     </head>
     <body>
@@ -30,7 +30,7 @@
       </footer>
 
     </div> <!-- /container -->
-    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script type="text/javascript">
         $("#hash").on("click", function ()
